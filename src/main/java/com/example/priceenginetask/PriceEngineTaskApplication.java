16cf5129f4bd95dad5e2789fce6719b41b60162f
artjom <artjom.kalita@gmail.com>
2021-05-09 19:25:05 +0300

    Initial commit

---
+++ src/main/java/com/example/priceenginetask/PriceEngineTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.priceenginetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceEngineTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceEngineTaskApplication.class, args);
+    }
+
+}
